f9f2833c3bb53d0532a0f73805a7ec0d9cd5b965
dangph12 <dangphhe181217@fpt.edu.vn>
2025-01-21 14:23:11 +0700

    chore: Remove an unused script

--- backend/set-env.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-if [ -r /run/secrets/db-password ]; then
-  export MYSQL_PASSWORD=$(cat /run/secrets/db-password)
-else
-  echo "Error: /run/secrets/db-password file not found or not readable"
-  exit 1
-fi
-
-exec "$@"
